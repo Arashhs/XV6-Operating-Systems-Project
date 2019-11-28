e5036ee2f24d6532d47b7bbe2e5546e3b41af65f
Arash <hajisafiarash@gmail.com>
2019-11-28 08:32:33 -0800

    Bugs fixed with sending pointer through pipe

--- Q1.c
+++ Q1.c
@@ -24,17 +24,19 @@ if(childpid  == -1) { //Error
 if(childpid == 0) { // 1256664876886345212
 	/* Child process */
 	// close(fd[0]);
+	close(fd1[1]);
+	close(fd2[0]);
+	
 	char getIn[50]; //input number
-	read(fd[0], getIn, sizeof(getIn));
+	read(fd1[0], getIn, sizeof(getIn));
 
-	printf("%s %d\n", getIn, sizeof(getIn));
 	int numSize = 0;
 	int i = 0;;
 	for(i=0; getIn[i] != '\0' ; ++i);
 	numSize = i;
-	printf("%d\n", numSize);
 	
-	int sumOfEvens = 0;
+	int sumOfEvens;
+	
 	
 	for(i=0; i<numSize; i++) {
 		int d = getIn[i] - '0';
@@ -42,7 +44,7 @@ if(childpid == 0) { // 1256664876886345212
 			sumOfEvens += d;
 	}
 
-	write(fd[1], sumOfEvens, sizeof(int));
+	write(fd2[1], &sumOfEvens, sizeof(getIn));
 	
 
 
@@ -52,16 +54,20 @@ if(childpid == 0) { // 1256664876886345212
 else {
 	/* Parent process */
 //	close(fd[1]);
+
+	close(fd1[0]);
+	close(fd2[1]);
+
 	char inputNum[50];
 	printf("Enter your number: ");
 	scanf("%s", inputNum);
 	
 
-	write(fd[1], inputNum, sizeof(inputNum));
+	write(fd1[1], inputNum, sizeof(inputNum));
 
 	int evensSum;
 	wait(3);
-	read(fd[0], evensSum, sizeof(int));
+	read(fd2[0], &evensSum, sizeof(inputNum));
 	printf("%s The sum of even digits in the input number :%d\n", inputNum, evensSum);
 
 	
