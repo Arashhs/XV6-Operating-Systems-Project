adbfec6afc33e12216488d2ed4f57447d2331b40
Arash <hajisafiarash@gmail.com>
2019-11-28 07:39:16 -0800

    Added pipe function to read and write between child and parent

--- Q1.c
+++ Q1.c
@@ -1,7 +1,38 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main()
 {
-printf("\nA sample C program\n\n");
+int fd[2];
+pid_t childpid;
+pipe(fd);
+
+
+
+childpid = fork();
+        
+if(childpid  == -1) { //Error
+	perror("fork");
+	exit(1);
+}
+
+if(childpid == 0) {
+	/* Child process closes up input side of pipe */
+	// close(fd[0]);
+	
+}
+
+else {
+	/* Parent process closes up output side of pipe */
+//	close(fd[1]);
+	char inputNum[50];
+	printf("Enter your number: ");
+	scanf("%s", inputNum);
+	printf("%s %d\n", inputNum, sizeof(inputNum));
+	write(fd[1], inputNum, sizeof(inputNum));
+	
+}
+
 return 0;
 }
