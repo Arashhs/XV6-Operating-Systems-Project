2c5d398e9121861c3d94a87226aaf570ca200135
Arash <hajisafiarash@gmail.com>
2019-11-28 08:17:46 -0800

    Pipe functionality implemented

--- Q1.c
+++ Q1.c
@@ -17,20 +17,49 @@ if(childpid  == -1) { //Error
 	exit(1);
 }
 
-if(childpid == 0) {
-	/* Child process closes up input side of pipe */
+if(childpid == 0) { // 1256664876886345212
+	/* Child process */
 	// close(fd[0]);
+	char getIn[50]; //input number
+	read(fd[0], getIn, sizeof(getIn));
+
+	printf("%s %d\n", getIn, sizeof(getIn));
+	int numSize = 0;
+	int i = 0;;
+	for(i=0; getIn[i] != '\0' ; ++i);
+	numSize = i;
+	printf("%d\n", numSize);
+	
+	int sumOfEvens = 0;
+	
+	for(i=0; i<numSize; i++) {
+		int d = getIn[i] - '0';
+		if (d%2 == 0)
+			sumOfEvens += d;
+	}
+
+	write(fd[1], sumOfEvens, sizeof(int));
+	
+
+
 	
 }
 
 else {
-	/* Parent process closes up output side of pipe */
+	/* Parent process */
 //	close(fd[1]);
 	char inputNum[50];
 	printf("Enter your number: ");
 	scanf("%s", inputNum);
-	printf("%s %d\n", inputNum, sizeof(inputNum));
+	
+
 	write(fd[1], inputNum, sizeof(inputNum));
+
+	int evensSum;
+	wait(3);
+	read(fd[0], evensSum, sizeof(int));
+	printf("%s The sum of even digits in the input number :%d\n", inputNum, evensSum);
+
 	
 }
 
